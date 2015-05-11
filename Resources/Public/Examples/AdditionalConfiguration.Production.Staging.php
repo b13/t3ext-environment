2b91e40baa09752c47495094746f215d244c7cc5
Martin Lauer <martin.lauer@b13.de>
2015-05-11 16:18:41 +0800

    [FIX] update default settings to match TYPO3 7.2 settings

--- Resources/Public/Examples/AdditionalConfiguration.Production.Staging.php
+++ Resources/Public/Examples/AdditionalConfiguration.Production.Staging.php
@@ -4,8 +4,21 @@ $GLOBALS['TYPO3_CONF_VARS']['DB']['host'] = '';
 $GLOBALS['TYPO3_CONF_VARS']['DB']['password'] = '';
 $GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = '';
 
+$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = '';
+$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = '';
+
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['cookieDomain'] = '.prev.host.tld';
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['curlUse'] = '1';
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '127.0.0.1,::1,yourIpHere';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['cookieSecure'] = '0';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = '2';
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = '1';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = '0';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = '1';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['systemLogLevel'] = '0';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['enableDeprecationLog'] = ''; 
+
+// see http://php.net/manual/en/errorfunc.constants.php
+// default values below
+//$GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] = '30466';
+//$GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = '28674';
+//$GLOBALS['TYPO3_CONF_VARS']['SYS']['syslogErrorReporting'] = '30711';
+//$GLOBALS['TYPO3_CONF_VARS']['SYS']['belogErrorReporting'] = '30711';
--- Resources/Public/Examples/AdditionalConfiguration.Production.php
+++ Resources/Public/Examples/AdditionalConfiguration.Production.php
@@ -4,9 +4,21 @@ $GLOBALS['TYPO3_CONF_VARS']['DB']['host'] = '';
 $GLOBALS['TYPO3_CONF_VARS']['DB']['password'] = '';
 $GLOBALS['TYPO3_CONF_VARS']['DB']['username'] = '';
 
+$GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = '';
+$GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = '';
+
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['cookieDomain'] = '.production.host.tld';
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['curlUse'] = '1';
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = 'yourIpHere';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['cookieSecure'] = '0';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = '0';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['sqlDebug'] = '0';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] = '';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['systemLogLevel'] = '2';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['enableDeprecationLog'] = '';
+
+// see http://php.net/manual/en/errorfunc.constants.php
+// default values below
+//$GLOBALS['TYPO3_CONF_VARS']['SYS']['errorHandlerErrors'] = '30466';
+//$GLOBALS['TYPO3_CONF_VARS']['SYS']['exceptionalErrors'] = '28674';
+//$GLOBALS['TYPO3_CONF_VARS']['SYS']['syslogErrorReporting'] = '30711';
+//$GLOBALS['TYPO3_CONF_VARS']['SYS']['belogErrorReporting'] = '30711';
